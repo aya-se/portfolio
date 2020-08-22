35e2822d1e6f8818723ca2e92b2d32d21c0141b5
aya-se <kakkeno1@gmail.com>
2020-08-23 04:31:43 +0900

    更新1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,8 +13,18 @@
           ></el-menu-item
         >
         <el-menu-item index="3"
+          ><a class="link-content" @click="clickSmoothScroll('#history')">
+            History</a
+          ></el-menu-item
+        >
+        <el-menu-item index="4"
           ><a class="link-content" @click="clickSmoothScroll('#skill')">
             Skill</a
+          ></el-menu-item
+        >
+        <el-menu-item index="5"
+          ><a class="link-content" @click="clickSmoothScroll('#work')">
+            Work</a
           ></el-menu-item
         >
       </el-menu>
@@ -54,8 +64,27 @@
       </div>
     </transition>
     <transition name="el-fade-in-linear">
+      <div id="history">
+        <h1><i class="el-icon-s-data" /> History</h1>
+        <el-timeline>
+          <el-timeline-item
+            class="timeline"
+            v-for="(activity, index) in activities"
+            :key="index"
+            :icon="activity.icon"
+            :type="activity.type"
+            :color="activity.color"
+            :size="activity.size"
+            :timestamp="activity.timestamp"
+          >
+            {{ activity.content }}
+          </el-timeline-item>
+        </el-timeline>
+      </div>
+    </transition>
+    <transition name="el-fade-in-linear">
       <div id="skill">
-        <h1><i class="el-icon-user-solid" /> Skill</h1>
+        <h1><i class="el-icon-s-opportunity" /> Skill</h1>
         <el-card class="box-card2">
           <h3>
             <label for="vue">Vue.js</label>
@@ -125,6 +154,54 @@
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      activities: [
+        {
+          content: "開成中学・高等学校を卒業する",
+          timestamp: "2019年3月初旬",
+          size: "large",
+          color: "gray"
+        },
+        {
+          content: "東京工業大学に入学する",
+          timestamp: "2019年4月初旬",
+          size: "large",
+          color: "gray"
+        },
+        {
+          content: "地元の個別指導塾で塾講師のバイトを始める (~2020年1月)",
+          timestamp: "2019年8月下旬",
+          size: "large",
+          color: "gray"
+        },
+        {
+          content: "AtCoderのレートが水色(1200)に到達する",
+          timestamp: "2019年11月下旬",
+          size: "large",
+          color: "gray"
+        },
+        {
+          content: "教育系の会社で事務(データ分析・資料作成等)のバイトを始める",
+          timestamp: "2019年12月上旬",
+          size: "large",
+          color: "gray"
+        },
+        {
+          content: "東京工業大学 情報工学系に進学する",
+          timestamp: "2019年4月初旬",
+          size: "large",
+          color: "gray"
+        },
+        {
+          content: "当ポートフォリオを開設する",
+          timestamp: "2019年8月下旬",
+          size: "large",
+          color: "gray"
+        }
+      ]
+    };
+  },
   components: {},
   methods: {
     clickSmoothScroll(x) {
@@ -135,6 +212,9 @@ export default {
 };
 </script>
 <style>
+#tab-menu {
+  margin: auto;
+}
 .link-content {
   size: 50px;
 }
@@ -152,5 +232,11 @@ export default {
 }
 .box-image {
   height: 160px;
+}
+.timeline {
+  width: 800px;
+  height: 100px;
+  font-size: 20px;
+  margin: auto;
 }
 </style>
