4112b16f1b89cabbb89855126f8801a89f723711
aya-se <kakkeno1@gmail.com>
2022-10-09 22:49:03 +0900

    SNSアイコンの移転

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,38 @@
         東京工業大学<br />情報理工学院 情報工学系 学部4年<br />岡崎研究室
       </div>
       <img class="top-icon" src="../assets/icon.png" />
+      <div class="sns-links-list">
+        <ul class="sns-links">
+          <li>
+            <a
+              href="https://twitter.com/Hakuba_snow"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/channel/UCW4FH8oKhHE8vIRnr0StJ8g"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/aya-se"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/kakeru-hattori-973404240/"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
+          </li>
+        </ul>
+      </div>
       <p class="top-description">
         岡崎研究室所属の服部と申します。
         <br />
@@ -18,7 +50,7 @@
       </p>
     </div>
     <div id="about">
-      <h1><i class="el-icon-info" /> About</h1>
+      <h2 class="subtitle"><i class="el-icon-info" />ABOUT</h2>
       <AboutCard
         v-for="(item, index) in abouts"
         :key="index"
@@ -28,19 +60,8 @@
       />
     </div>
 
-    <div id="account">
-      <h1><i class="el-icon-menu" /> Account</h1>
-      <AccountCard
-        v-for="(item, index) in accounts"
-        :key="index"
-        :title="item.title"
-        :link="item.link"
-        :image="item.image"
-      />
-    </div>
-
     <div id="history">
-      <h1><i class="el-icon-s-data" /> History</h1>
+      <h2 class="subtitle"><i class="el-icon-s-data" />HISTORY</h2>
       <el-timeline>
         <el-timeline-item
           class="timeline"
@@ -58,7 +79,7 @@
     </div>
 
     <div id="skill">
-      <h1><i class="el-icon-s-opportunity" /> Skill</h1>
+      <h2 class="subtitle"><i class="el-icon-s-opportunity" />SKILL</h2>
       <SkillCard
         v-for="(item, index) in skills"
         :key="index"
@@ -70,7 +91,7 @@
     </div>
 
     <div id="work">
-      <h1><i class="el-icon-s-claim" /> Work</h1>
+      <h2 class="subtitle"><i class="el-icon-s-claim" />WORKS</h2>
       <WorkCard
         v-for="(item, index) in works"
         :key="index"
@@ -84,7 +105,7 @@
     </div>
 
     <div id="contact">
-      <h1><i class="el-icon-s-comment" /> Contact</h1>
+      <h2 class="subtitle"><i class="el-icon-s-comment" />CONTACT</h2>
       <p><b>Gmail： </b>kakkeno1@gmail.com</p>
       <p><b>Twitter： </b>@Hakuba_snow</p>
     </div>
@@ -95,12 +116,10 @@
 import { defineComponent } from "vue";
 import CustomHeader from "@/components/Header.vue";
 import AboutCard from "@/components/AboutCard.vue";
-import AccountCard from "@/components/AccountCard.vue";
 import SkillCard from "@/components/SkillCard.vue";
 import WorkCard from "@/components/WorkCard.vue";
 import activities from "@/data/activities.json";
 import abouts from "@/data/abouts.json";
-import accounts from "@/data/accounts.json";
 import skills from "@/data/skills.json";
 import works from "@/data/works.json";
 export default defineComponent({
@@ -109,12 +128,11 @@ export default defineComponent({
     return {
       abouts: abouts,
       activities: activities,
-      accounts: accounts,
       skills: skills,
       works: works,
     };
   },
-  components: { CustomHeader, AboutCard, AccountCard, SkillCard, WorkCard },
+  components: { CustomHeader, AboutCard, SkillCard, WorkCard },
   methods: {
     onClick(url) {
       window.open(url, "_blank");
@@ -126,9 +144,17 @@ export default defineComponent({
 #home {
   overflow: hidden;
 }
-#top {
+#top,
+#experience,
+#skill,
+#contact {
   padding: 10px 20px 30px;
   background: #fafafa;
+}
+#about,
+#history,
+#work {
+  padding: 10px 20px 30px;
 }
 .tab {
   position: fixed;
@@ -136,6 +162,13 @@ export default defineComponent({
   width: 100%;
   z-index: 100;
   background-color: white;
+}
+.subtitle {
+  color: #333333;
+  font-size: 36px;
+  font-family: "Open Sans", serif;
+  letter-spacing: 0.05em;
+  font-weight: normal;
 }
 .top-title {
   font-size: 36px;
@@ -147,6 +180,12 @@ export default defineComponent({
   color: #4f4f4f;
   margin: 20px 0;
   line-height: 175%;
+}
+.sns-links-list {
+  margin: 20px 0;
+  display: flex;
+  flex-wrap: warp;
+  justify-content: center;
 }
 .top-description {
   text-align: left;
@@ -197,5 +236,94 @@ export default defineComponent({
   margin-left: 5px;
   margin-right: 5px;
   margin-top: 5px;
+}
+.sns-links {
+  list-style: none;
+  overflow: hidden;
+  padding: 0;
+  margin: 0;
+}
+.sns-links li {
+  float: left;
+  margin: 0 8px;
+  padding: 0;
+}
+.sns-links a {
+  text-decoration: none;
+}
+.sns-links li a::before {
+  border-style: solid;
+  border-width: 1px;
+  -webkit-border-radius: 2px;
+  border-radius: 2px;
+  color: #fff;
+  display: inline-block;
+  font-family: FontAwesome;
+  font-size: 16px;
+  height: 42px; /* Button height */
+  line-height: 42px; /* Button height */
+  -webkit-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+  text-align: center;
+  width: 42px; /* Button width */
+}
+.sns-links li a[href*="facebook.com"]::before {
+  border-color: #3b5998;
+  background-color: #3b5998;
+  content: "\f09a";
+}
+.sns-links li a[href*="github.com"]::before {
+  border-color: #181717;
+  background-color: #181717;
+  content: "\f09b";
+}
+.sns-links li a[href*="plus.google.com"]::before {
+  border-color: #dc4e41;
+  background-color: #dc4e41;
+  content: "\f0d5";
+}
+.sns-links li a[href*="linkedin.com"]::before {
+  border-color: #0077b5;
+  background-color: #0077b5;
+  content: "\f0e1";
+}
+.sns-links li a[href*="slideshare.net"]::before {
+  border-color: #2d2d2d;
+  background-color: #2d2d2d;
+  content: "\f1e7";
+}
+.sns-links li a[href*="twitter.com"]::before {
+  border-color: #55acee;
+  background-color: #55acee;
+  content: "\f099";
+}
+.sns-links li a[href*="youtube.com"]::before {
+  border-color: #cd201f;
+  background-color: #cd201f;
+  content: "\f16a";
+}
+.sns-links li a:hover::before {
+  background-color: #fff;
+}
+.sns-links li a[href*="facebook.com"]:hover::before {
+  color: #3b5998;
+}
+.sns-links li a[href*="github.com"]:hover::before {
+  color: #181717;
+}
+.sns-links li a[href*="plus.google.com"]:hover::before {
+  color: #dc4e41;
+}
+.sns-links li a[href*="linkedin.com"]:hover::before {
+  color: #0077b5;
+}
+.sns-links li a[href*="slideshare.net"]:hover::before {
+  color: #2d2d2d;
+}
+.sns-links li a[href*="twitter.com"]:hover::before {
+  color: #55acee;
+}
+.sns-links li a[href*="youtube.com"]:hover::before {
+  color: #cd201f;
 }
 </style>
